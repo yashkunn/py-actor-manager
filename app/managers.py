84ceb782c247aec67a6335128c6afb0106a2a4fc
Yakiv Lobach <lobachyakiv@gmail.com>
2024-08-23 09:52:18 +0300

    second_try

--- app/managers.py
+++ app/managers.py
@@ -5,16 +5,13 @@ from models import Actor
 
 class ActorManager:
     def __init__(self) -> None:
-        self._connection = sqlite3.connect(
-            "C:\\Users\\lobac\\environment_setup\\"
-            "py-actor-manager\\cinema.sqlite"
-        )
+        self._connection = sqlite3.connect("cinema.sqlite")
         self.table_name = "actors"
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} "
-            f"(first_name, last_name) VALUES (?, ?)",
+            f"""INSERT INTO {self.table_name}
+            (first_name, last_name) VALUES (?, ?)""",
             (first_name, last_name)
         )
         self._connection.commit()
@@ -29,9 +26,9 @@ class ActorManager:
 
     def update(self, id_: int, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} "
-            f"SET first_name = ?, last_name = ? "
-            f"WHERE id = ?",
+            f"""UPDATE {self.table_name}
+            SET first_name = ?, last_name = ?
+            WHERE id = ?""",
             (first_name, last_name, id_)
         )
         self._connection.commit()
